27c8799fff9b16c1a793fe75183a0df89e7510aa
guettafa <irontitou5@gmail.com>
2024-06-18 09:25:31 -0400

    initial commit

---
+++ src/main/java/com/pekilla/PekillaApplication.java
@@ -0,0 +1,13 @@
+package com.pekilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PekillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PekillaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pekilla/PekillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pekilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PekillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
