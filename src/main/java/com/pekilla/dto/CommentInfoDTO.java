84318ed7bf24925efe336017e7cb8789bdab9c2a
guettafa <irontitou5@gmail.com>
2024-06-29 14:55:14 -0400

    Transformed CommentInfoDTO from class to record

--- src/main/java/com/pekilla/dto/CommentInfoDTO.java
+++ src/main/java/com/pekilla/dto/CommentInfoDTO.java
@@ -1,13 +1,10 @@
 package com.pekilla.dto;
 
 import lombok.Builder;
-import lombok.Data;
 
 @Builder
-@Data
-public class CommentInfoDTO {
-
-    private String message;
-    private long postId;
-    private long userId;
-}
+public record CommentInfoDTO (
+        String message,
+        long postId,
+        long userId)
+{ }
