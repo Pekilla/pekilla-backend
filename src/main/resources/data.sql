b5c155b951a4e5489bbf0aa440c23a55319dac31
AchrafGroiez <41470d0a@gmail.com>
2024-06-29 22:50:49 -0400

    Update data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO user(id, is_active, password, username, link) VALUES (1, true, 'sVAJjugOwwhJJSFdFRMa', 'Farid', '111111111');
+INSERT INTO user(id, is_active, password, username, link) VALUES (2, false, 'dYauqOCHNSFlNwSdHRQ', 'Maxime', '111111112');
+INSERT INTO user(id, is_active, password, username, link) VALUES (3, true, 'vHpNNigeBxwElLJnJrOO', 'Jacques', '111111113');
+INSERT INTO user(id, is_active, password, username, link) VALUES (4, true, 'JBCJKvKFQcIKuTotidly', 'Jean', '111111114');
+INSERT INTO user(id, is_active, password, username, link) VALUES (5, true, 'MXjdBWFLYYmAfvsWLrcz', 'Kim', '111111115');
+INSERT INTO user(id, is_active, password, username, link) VALUES (6, false, 'prpYFrOXbblPMazuaYc', 'Carl', '111111116');
+INSERT INTO user(id, is_active, password, username, link) VALUES (7, true, 'ShKYVcgJhPOsPGyTYNaW', 'Sam', '111111117');
+
+INSERT INTO post (added_date, original_poster_id, title, description, category) VALUES ('2019-04-03 00:00:00', 1, 'Chat GPT 4', 'The new revolution...', 'PROGRAMMING');
+INSERT INTO post (added_date, original_poster_id, title, description, category) VALUES ('2012-08-23 00:00:00', 1, 'React Hooks', 'The new hook...', 'PROGRAMMING');
+INSERT INTO post (added_date, original_poster_id, title, description, category) VALUES ('2002-02-01 00:00:00', 1, 'How to make the compiler...', 'When the compiler...', 'PROGRAMMING');
