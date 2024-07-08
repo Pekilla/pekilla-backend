9b24a2add92bda624dba87c27ce44779c38fbf17
AchrafGroiez <41470d0a@gmail.com>
2024-07-07 20:29:42 -0400

    Added attribute in User model.

--- src/main/java/com/pekilla/model/User.java
+++ src/main/java/com/pekilla/model/User.java
@@ -2,9 +2,12 @@ package com.pekilla.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+
+import java.time.LocalDate;
 
 @EqualsAndHashCode(callSuper = true)
 @Builder
@@ -22,4 +25,20 @@ public class User extends ForumTable {
 
     @NotBlank
     private String password;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    private String icon;
+    private String banner;
+
+    // For the future, to know if as user as entered a code that he received by email.
+    @Builder.Default
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT false")
+    private Boolean isActivated = false;
+
+    @Builder.Default
+    @Column(nullable = false, columnDefinition = "DATETIME DEFAULT NOW()")
+    private LocalDate joinedDate = LocalDate.now();
 }
